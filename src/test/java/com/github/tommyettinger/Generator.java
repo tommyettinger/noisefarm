6b0b0edda42ca29937eb25620493b0ff361f5eab
Tommy Ettinger <tommy.ettinger@gmail.com>
2020-11-07 21:56:25 -0800

    Add equalized-range noise, which should always have black and white

--- src/test/java/com/github/tommyettinger/Generator.java
+++ src/test/java/com/github/tommyettinger/Generator.java
@@ -19,8 +19,7 @@ public class Generator extends ApplicationAdapter {
     @Override
     public void create() {
     	noise = new Noise(1, 2f, Noise.SIMPLEX_FRACTAL, 2);
-        Gdx.files.local("images").mkdirs();	       
-        renderPNG();
+        renderEqualizedPNG();
         Gdx.app.exit();
     }
     
@@ -35,7 +34,7 @@ public class Generator extends ApplicationAdapter {
 		png.setFlipY(false);
 		png.setCompression(7);
 		int size = 256;
-
+		
 		Pixmap map = new Pixmap(size, size, Pixmap.Format.RGB888);
 		ByteBuffer buf = map.getPixels();
 
@@ -65,6 +64,62 @@ public class Generator extends ApplicationAdapter {
 							}
 							try {
 								png.write(Gdx.files.local(String.format("generated/%6$dx%6$d/%s/%s/%d-octaves-%d-frequency_%02d.png", noiseType, fractal, octaves, frequency, seed, size)), map);
+							} catch (IOException e) {
+								Gdx.app.error("png", e.getMessage());
+							}
+							buf.rewind();
+						}
+					}
+				}
+			}
+		}
+	}
+    public void renderEqualizedPNG() {
+		PixmapIO.PNG png = new PixmapIO.PNG();
+		png.setFlipY(false);
+		png.setCompression(7);
+		int size = 256;
+		
+		float[][] raw = new float[size][size];
+
+		Pixmap map = new Pixmap(size, size, Pixmap.Format.RGB888);
+		ByteBuffer buf = map.getPixels();
+
+		String[] noiseTypes = {"value", "perlin", "simplex", "foam", "honey"};
+		int[] noiseTypeIndices = {Noise.VALUE_FRACTAL, Noise.PERLIN_FRACTAL, Noise.SIMPLEX_FRACTAL, Noise.FOAM_FRACTAL, Noise.HONEY_FRACTAL};
+		String[] fractals = {"fbm", "billow", "ridged"};
+		String noiseType, fractal;
+		for (int nti = 0; nti < noiseTypeIndices.length; nti++) {
+			noiseType = noiseTypes[nti];
+			noise.setNoiseType(noiseTypeIndices[nti]);
+			for (int fi = 0; fi < fractals.length; fi += 2) {
+				fractal = fractals[fi];
+				noise.setFractalType(fi);
+				for (int octaves = 1; octaves <= 3; octaves++) {
+					noise.setFractalOctaves(octaves);
+					for (int frequency = 1; frequency <= 8; frequency *= 2) {
+						noise.setFrequency(frequency);
+						for (int seed = 0; seed < 16; seed++) {
+							noise.setSeed(seed);
+							float min = Float.MAX_VALUE, max = -Float.MAX_VALUE, t;
+							for (int y = 0; y < size; y++) {
+								for (int x = 0; x < size; x++) {
+									raw[y][x] = t = noise.seamless2D(x, y, size, size, seed);
+									min = Math.min(min, t);
+									max = Math.max(max, t);
+								}
+							}
+							float range = 255.999f / (max - min);
+							for (int y = 0; y < size; y++) {
+								for (int x = 0; x < size; x++) {
+									byte b = (byte)(range * (raw[y][x] - min));
+									buf.put(b);
+									buf.put(b);
+									buf.put(b);
+								}
+							}
+							try {
+								png.write(Gdx.files.local(String.format("generated/%6$dx%6$dEq/%s/%s/%d-octaves-%d-frequency_%02d.png", noiseType, fractal, octaves, frequency, seed, size)), map);
 							} catch (IOException e) {
 								Gdx.app.error("png", e.getMessage());
 							}
